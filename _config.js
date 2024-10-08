b165a4d7c9c1f6a63004f373d8e4959c0eaee9f2
Naspwon <naominyongesa12@gmail.com>
2024-10-08 21:36:43 +0300

    Updated mongodb config

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    // production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    // development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    // test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+
+    production: 'mongodb+srv://Nayomie:Welcome@123@nash.j03th.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Nash',
+    development: 'mongodb+srv://Nayomie:Welcome@123@nash.j03th.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Nash',
+    test: 'mongodb+srv://Nayomie:Welcome@123@nash.j03th.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Nash',
 }
 module.exports = config;
